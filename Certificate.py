8be009faea24ee4d117a2a79393f00d12d472f62
AhmedArfaouii <ahmed.arfaouii11@gmail.com>
2023-10-26 18:33:33 +0100

    Added working enc/dec with cert

--- Certificate.py
+++ Certificate.py
@@ -1,4 +1,8 @@
 from OpenSSL import crypto
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.backends import default_backend  
+from cryptography import x509
 
 def cert_gen(
     emailAddress="ahmed.arfaouii11@gmail.com",
@@ -32,4 +36,45 @@ def cert_gen(
     with open(KEY_FILE, "wt") as f:
         f.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, k).decode("utf-8"))
 
-cert_gen()
+def encrypt_w_cert () :
+    with open("selfsigned.crt","rb") as cert_file:
+        certificate = x509.load_pem_x509_certificate(cert_file.read(), default_backend())
+    public_key = certificate.public_key()
+    
+    message = b"Your message to encrypt"
+
+    encrypted_message = public_key.encrypt(
+        message,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    f =open ("encrypted_w_cert.txt","wb")
+    f.write(encrypted_message)
+    f.close
+    
+def decrypt_w_cert():
+
+    with open("private.key", "rb") as key_file:
+        private_key = serialization.load_pem_private_key(
+            key_file.read(),
+            password=None,
+            backend=default_backend()
+        )
+
+    with open("encrypted_w_cert.txt", "rb") as encrypted_file:
+        encrypted_message = encrypted_file.read()
+
+    decrypted_message = private_key.decrypt(
+        encrypted_message,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+    print(decrypted_message.decode())
+    
